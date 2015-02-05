f0ab92fc1df1ba01d4d6b5473e14d62d09d849c2
Diego Mazzone <mazzone.diego@gmail.com>
2015-02-05 02:15:44 -0200

    Initial Commit

---
+++ Maps/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Maps
+//
+//  Created by Diego Mazzone on 5/2/15.
+//  Copyright (c) 2015 MundoMac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
